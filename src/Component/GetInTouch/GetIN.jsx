4e82b4384d205ca4ff8d6eaf331e67f0876f3330
atinuke ilesanmi <atinukeilesanmi921@gmail.com>
2024-04-04 15:53:50 +0100

    d

---
+++ src/Component/GetInTouch/GetIN.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import style from "./GetIN.module.css";
+
+export default GetIN = () => {
+  return (
+    <>
+      <div>
+        <h1>Get In Touch</h1>
+        <label> Contact me</label>
+      </div>
+    </>
+  );
+};
